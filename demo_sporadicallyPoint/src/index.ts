c8fc0ec4b2030d765d099dd28dfdc0313d0f017b
tutulyy <lyyoicq@163.com>
2018-05-07 12:39:34 +0800

    point

--- demo_sporadicallyPoint/src/index.ts
+++ demo_sporadicallyPoint/src/index.ts
@@ -5,13 +5,16 @@
 //  let fragment_shader = require('./shader/fragment-1.glsl');
 
 
-import * as  vertex_shader from'./shader/vertex.glsl';
-import * as  fragment_shader from'./shader/fragment-1.glsl';
-console.log("vertex_shader");
-console.log(vertex_shader);
+import * as  vertex_shader from './shader/vertex.glsl';
+import * as  fragment_shader from './shader/fragment-1.glsl';
+
+import * as  vertexPoint_shader from './shader/vertexPoint.glsl';
+import * as  fragmentPoint_shader from './shader/fragmentPoint.glsl';
+console.log("vertexPoint_shader");
+console.log(vertexPoint_shader);
 
 
-import * as THREE from 'three' ;
+import * as THREE from 'three';
 import { Material } from 'three';
 import WebGLInit from './init'
 
@@ -23,11 +26,11 @@ let CreateMaterial = (vertexShader, fragmentShader) => {
 
     var attributes = {};
     var uniforms = {
-        sleep: {type: 'f', value: 3},
-        time: {type: 'f', value: 0.2},
-        scale: {type: 'f', value: 0.2},
-        alpha: {type: 'f', value: 0.6},
-        resolution: {type: "v2", value: new THREE.Vector2()}
+        sleep: { type: 'f', value: 3 },
+        time: { type: 'f', value: 0.2 },
+        scale: { type: 'f', value: 0.2 },
+        alpha: { type: 'f', value: 0.6 },
+        resolution: { type: "v2", value: new THREE.Vector2() }
     };
 
     uniforms.resolution.value.x = window.innerWidth;
@@ -43,35 +46,65 @@ let CreateMaterial = (vertexShader, fragmentShader) => {
     return meshMaterial;
 }
 
+let pointGeometry = new THREE.Geometry();
+let pointVertext = new THREE.Vector3(20, 20, 20);  
+pointGeometry.vertices.push(pointVertext);
+var uniforms = {
+    sleep: { type: 'f', value: 3 },
+    time: { type: 'f', value: 0.2 },
+    scale: { type: 'f', value: 0.2 },
+    alpha: { type: 'f', value: 0.6 },
+    resolution: { type: "v2", value: new THREE.Vector2() }
+};
+uniforms.resolution.value.x = window.innerWidth;
+uniforms.resolution.value.y = window.innerHeight;
+let pointMaterial = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: String(vertexPoint_shader),
+    fragmentShader: String(fragmentPoint_shader),
+    transparent: true
+});
+let points=new THREE.Points(pointGeometry, pointMaterial);
+webGLInit.scene.add(points);//点模型添加到场景中
 
-let cubeGeometry = new THREE.BoxGeometry(20, 20, 20);
-let meshMaterial1 = CreateMaterial("vertex-shader", "fragment-shader-1");
-let material = [
-    meshMaterial1,
-    meshMaterial1,
-    meshMaterial1,
-    meshMaterial1,
-    meshMaterial1,
-    meshMaterial1
-];
 
-let cube = new THREE.Mesh(cubeGeometry, material);
-webGLInit.scene.add(cube);
+
+// let cubeGeometry = new THREE.BoxGeometry(20, 20, 20);
+// let meshMaterial1 = CreateMaterial("vertex-shader", "fragment-shader-1");
+// let material = [
+//     meshMaterial1,
+//     meshMaterial1,
+//     meshMaterial1,
+//     meshMaterial1,
+//     meshMaterial1,
+//     meshMaterial1
+// ];
+
+// let cube = new THREE.Mesh(cubeGeometry, material);
+//webGLInit.scene.add(cube);
 webGLInit.scene.add(webGLInit.camera);
 webGLInit.scene.add(webGLInit.ambientLight);
 webGLInit.scene.add(webGLInit.spotLight);
 
- let step = 0;
+let step = 0;
 
- function render() {
-    cube.rotation.y = step += 0.001;
-    cube.rotation.x = step;
-    cube.rotation.z = step;
+const lifeCycle = 5;
+function render() {
+    // cube.rotation.y = step += 0.001;
+    // cube.rotation.x = step;
+    // cube.rotation.z = step;
 
-    (cube.material as Array<THREE.ShaderMaterial>).forEach(function (e) {
-        e.uniforms.time.value += 0.01;
-    });
-
+    // (cube.material as Array<THREE.ShaderMaterial>).forEach(function (e) {
+    //     e.uniforms.time.value += 0.01;
+    // });
+    step += 0.001
+    points.rotation.y = step;
+    //points.rotation.x = step;
+    //points.rotation.z = step;
+    (points.material as THREE.ShaderMaterial).uniforms.time.value += 0.01;
+    // cube.material = (cube.material as Array<THREE.ShaderMaterial>).filter(
+    //     (v) => { return v.uniforms.time.value < lifeCycle }
+    // );
     // render using requestAnimationFrame
     requestAnimationFrame(render);
     webGLInit.renderer.render(webGLInit.scene, webGLInit.camera);
---
+++ demo_sporadicallyPoint/src/shader/vertexPoint.glsl
@@ -0,0 +1,16 @@
+uniform float time;
+varying vec2 vUv;
+
+
+void main()
+{
+    gl_PointSize = 100.;
+    vec3 posChanged = position;
+    posChanged.x = posChanged.x*(abs(sin(time*0.10)));
+    posChanged.y = posChanged.y*(abs(cos(time*0.10)));
+    posChanged.z = posChanged.z*(abs(sin(time*0.10)));
+    //gl_Position = projectionMatrix * modelViewMatrix * vec4(position*(abs(sin(time)/2.0)+0.5),1.0);
+    //gl_Position = position;
+    //gl_Position = projectionMatrix * modelViewMatrix * vec4(posChanged,1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(posChanged,1.0);
+}
