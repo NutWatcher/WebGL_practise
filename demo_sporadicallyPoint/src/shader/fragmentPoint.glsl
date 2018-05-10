43faee5bbc1576cd5fdb6fede5749165ef275b9e
tutulyy <lyyoicq@163.com>
2018-05-10 18:42:45 +0800

    notes

--- demo_sporadicallyPoint/src/shader/fragmentPoint.glsl
+++ demo_sporadicallyPoint/src/shader/fragmentPoint.glsl
@@ -8,6 +8,8 @@ void main(void)
     float d = distance(gl_PointCoord, vec2(0.5,0.5));
     if(d < 0.5){ //判断距离，如果小于0.5就绘制
     }
+
+    // y = 1/x 
     if ( (1.0 / (5.0* ((abs(gl_PointCoord.x - 0.5) + 0.25)))) - 0.25  > abs(gl_PointCoord.y - 0.5)){
         gl_FragColor = vec4(
         1,
